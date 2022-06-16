f56a533b76d5a8924cc8c40fa8a1e734765bdb45
Meet Patel <meetnpatel368@gmail.com>
2022-06-16 00:45:58 -0400

    Separated modal and emoji component

---
+++ hyper-note-frontend-new/src/components/emoji_component/EmojiComponent.tsx
@@ -0,0 +1,20 @@
+import { IEmojiData } from 'emoji-picker-react';
+import React, { useContext } from 'react'
+import { context } from '../../state_manager/reducers/userState';
+import dynamic from 'next/dynamic';
+const Picker = dynamic(() => import('emoji-picker-react'), { ssr: false });
+
+const EmojiComponent = () => {
+    const { state, dispatch } = useContext(context);
+    const handleEmoji = (e: React.MouseEvent<Element, MouseEvent>, emojiObj: IEmojiData) => {
+        dispatch({ type: 'UPDATE_ICON', payload: {id: state.selected.id, emoji: emojiObj.emoji} })
+    }
+    return (
+        <div className='absolute z-30' onClick={(e) => e.stopPropagation()}>
+            <Picker onEmojiClick={handleEmoji} ></Picker>
+            
+        </div>
+    )
+}
+
+export default EmojiComponent
---
+++ hyper-note-frontend-new/src/components/modal_components/ModalCover.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+    children: JSX.Element,
+    handleClick: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ModalCover = ({handleClick, children}:Props) => {
+    return (
+        <div>
+            <div className='fixed z-20 top-0 left-0 w-full h-screen' onClick={() => handleClick(false)}> </div>
+            {/* <div className='fixed z-20 top-0 bg-pink-300 left-0 w-full h-screen' onClick={()=> handleClick(false)}></div> */}
+            {children}
+        </div>
+    )
+}
+
+export default ModalCover
