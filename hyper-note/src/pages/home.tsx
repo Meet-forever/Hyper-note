2ff65b1f9ec1f8d7ce1db66d490a7675bc5db4e3
Meet Patel <meetnpatel368@gmail.com>
2022-07-15 23:25:40 -0400

    deployment

--- hyper-note/src/pages/home.tsx
+++ hyper-note/src/pages/home.tsx
@@ -7,20 +7,21 @@ import { MultiContextProvider } from '../state_manager/index'
 import { preferenceReducerFunction, initialPreference } from '../state_manager/preference'
 import { getinitialSidebarList, sideBarListReducerFunction } from '../state_manager/sidebarList'
 import axios from "axios"
-const home = ({ session, userlist, theme_images }: { session: any, userlist: any, theme_images: string[] }) => {
-    const preference = useReducer(preferenceReducerFunction, {...initialPreference, theme_images: theme_images})
+import { getToken } from 'next-auth/jwt'
+const home = ({ token, userlist, theme_images }: { token: any, userlist: any, theme_images: string[] }) => {
+    const preference = useReducer(preferenceReducerFunction, { ...initialPreference, theme_images: theme_images })
     const sidebarList = useReducer(sideBarListReducerFunction, getinitialSidebarList(userlist.notes))
     const multireducers = {
         preference,
         sidebarList
     }
-    axios.defaults.headers.common["Authorization"] = `Bearer ${session.accessToken}`
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token.accessToken}`
     const { status, data } = useSession();
     useEffect(() => {
         const updatesidebarlist = setTimeout(() => {
             axios.post("/api/updatesidebarlist", {
                 notelist: sidebarList[0],
-                provider: session.provider
+                provider: token.provider
             })
         }, 2000)
         return () => clearTimeout(updatesidebarlist)
@@ -40,30 +41,41 @@ const home = ({ session, userlist, theme_images }: { session: any, userlist: any
 }
 
 export async function getServerSideProps(context: any) {
-    const path = await import("path")
-    const fs = await import("fs/promises")
-    const imgfolder = path.join(
-        ...(__dirname
-        .split(path.sep)
-        .slice(0, -3)
-        .concat(["public", "images", "themes"])))
-    const theme_images = await fs.readdir(imgfolder)
-    const session = await getSession(context);
-    if (!session) return {
+    const theme_images = [
+        'i1.jpg', 'mt-fuji.jpg',
+        'onepiece.jpg', 'samurai.jpg',
+        'store.jpg', 'test.jpg',
+        'test1.jpg', 'test2.jpg',
+        'test3.jpg', 'test4.jpg',
+        'test5.jpg', 'test6.png',
+        'wave.jpg'
+    ]
+    // const path = await import("path")
+    // const fs = await import("fs/promises")
+    // const imgfolder = path.join(
+    //     ...(__dirname
+    //     .split(path.sep)
+    //     .slice(0, -3)
+    //     .concat(["public", "images", "themes"])))
+    // console.log(await fs.readdir(imgfolder))
+    const {req} = context
+    const secret = process.env.SECRET
+    const token = await getToken({req: req, secret: secret});
+    if (!token) return {
         redirect: {
             destination: '/',
             permanent: false
         }
     }
-    const BEARER = "Bearer " + session.accessToken
-    const userlist = await fetch('http://localhost:3000/api/notelist', {
+    const BEARER = "Bearer " + token.accessToken
+    const userlist = await fetch(`${process.env.NEXTAUTH_URL}/api/notelist`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             'Authorization': BEARER
         },
-        body:JSON.stringify({
-            provider: session.provider
+        body: JSON.stringify({
+            provider: token.provider
         })
     }).then(res => res.json()).then(i => i).catch(err => console.error(err.message))
     if (!userlist || !("notes" in userlist)) return {
@@ -72,7 +84,7 @@ export async function getServerSideProps(context: any) {
             permanent: false
         }
     }
-    return { props: { session, userlist, theme_images } };
+    return { props: { token, userlist, theme_images } };
 }
 
 export default home
